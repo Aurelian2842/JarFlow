8415f2b92f80a29b11237da1a986bbac451d7b8d
Aurelian2842 <aurelian2842@gmail.com>
2025-10-03 08:23:36 +0800

    feat: publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+import org.jreleaser.model.Active
+
 plugins {
     id("java")
     id("com.gradleup.shadow") version "8.3.1"
     id("maven-publish")
+    id("org.jreleaser") version "1.20.0"
 }
 
 group = "com.aurelian2842.jarflow"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -31,10 +34,65 @@ publishing {
             version = project.version.toString()
 
             from(components["java"])
+
+            pom {
+                name = project.name
+                description = "A library used to load dependencies dynamically"
+                url = "https://github.com/Aurelian2842/JarFlow"
+                inceptionYear = "2025"
+                licenses {
+                    license {
+                        name = "LGPL-3.0-or-later"
+                        url = "https://spdx.org/licenses/LGPL-3.0-or-later.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "aurelian2842"
+                        name = "Aurelian2842"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/Aurelian2842/JarFlow.git"
+                    developerConnection = "scm:git:ssh://github.com/Aurelian2842/JarFlow.git"
+                    url = "http://github.com/Aurelian2842/JarFlow"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
         }
     }
 }
 
 tasks.publish {
     dependsOn(tasks.named("publishMavenPublicationToMavenLocal"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
+}
+
+jreleaser {
+    signing {
+        active = Active.ALWAYS
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active = Active.ALWAYS
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                }
+            }
+        }
+    }
 }
